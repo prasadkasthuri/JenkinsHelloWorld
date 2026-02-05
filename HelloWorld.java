24623278b72eb15e28321c62acd1069810668411
prasadkasthuri <prasad.kasthuri1@gmail.com>
2026-02-05 07:25:43 +0530

    Update print statement for Jenkins execution message

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!, Jenkins execution using hook polling!!"); 
+        System.out.println("Hello, World!, Jenkins using Webhook polling!!"); 
     }
 }
